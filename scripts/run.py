3dbb1107334ffc2125769361cb4ca4618d99a9a1
Dev0011111000101010 <62564629+Dev0011111000101010@users.noreply.github.com>
2022-11-06 15:32:43 -0500

    Change path settings from virtual machine to local
    The result of success is: the path of the variable feedback_txt_files_folder_path worked
    
    Visual confirmation: print(os.listdir(feedback_txt_files_folder_path)) command worked
    
    In the form of a list of files containing reviews for the site:
    #QA: Result: If everything is correct, it should return a list of TXT feedback files.
    ['feedback001.txt', 'feedback003.txt', 'feedback002.txt']

--- scripts/run.py
+++ scripts/run.py
@@ -4,8 +4,13 @@ import os
 import requests
 
 
-# This folder stores TXT files feedback for the site
-feedback_txt_files_folder_path = '/data/feedback'
+## This folder stores TXT files feedback for the site
+## The correct code to use on the combat server to complete the task
+## The lines below need to be uncommented and applied when running the qwiklabs task
+# feedback_txt_files_folder_path = '/data/feedback'
+## The line below was created for local testing without creating a load on the qwiklabs Google server
+## The line below should be commented out when running the job online in qwiklabs, while the line above is uncommented
+feedback_txt_files_folder_path = '/Users/il/PycharmProjects/Qwiklabs--Process-Text-Files-with-Python-Dictionaries-and-Upload-to-Running-Web-Service-using-Djang/test_feedback'
 
 # Get the list of all files and directories
 # in the 'feedback_txt_files_folder_path' directory
