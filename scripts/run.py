c50f2af700f73a709f272a2dfefb579489885b6a
Dev0011111000101010 <62564629+Dev0011111000101010@users.noreply.github.com>
2022-11-10 20:57:14 -0500

    Added VM web server console response
    At the end of the file, the response of the VM server to run the script is added = all sequentially displayed debug messages

--- scripts/run.py
+++ scripts/run.py
@@ -97,3 +97,104 @@ def txt_2_dictionary_jungo_structure(feedback_txt_files_folder_path):
 
 # Launching the "txt_2_dictionary_jungo_structure" function into which the "feedback_txt_files_folder_path" path is passed
 convert_txt_feedbacks_2_dictionary_jungo_structure = txt_2_dictionary_jungo_structure(feedback_txt_files_folder_path)
+
+
+# Server console response
+"""
+student-00-1f0759f0151d@corpweb:~$ ./run.py
+----------------------
+----------------------
+# QA: Result: If everything is correct, it should return a list of TXT feedback files.
+['001.txt', '019.txt', '020.txt', '007.txt', '005.txt']
+----------------------
+----------------------
+----------------------
+# QA: Result: If everything is correct, it should return a list of TXT feedback files.
+# Difference from above: This test is already running from the "txt_2_dictionary_jungo_structure" function
+['001.txt', '019.txt', '020.txt', '007.txt', '005.txt']
+----------------------
+001.txt    # QA : Iterating over each individual feedback file
+019.txt    # QA : Iterating over each individual feedback file
+020.txt    # QA : Iterating over each individual feedback file
+007.txt    # QA : Iterating over each individual feedback file
+005.txt    # QA : Iterating over each individual feedback file
+----------------------
+----------------------
+# QA : Iterating over each individual feedback file content
+# QA : The second line shows the converted dictionary
+['Great Customer Service\n', 'John\n', '2017-12-21\n', 'The customer service here is very good. They helped me find a 2017 Camry with good condition in reasonable price. Campared to other dealers, they provided the lowest price. Definttely recommend!\n']
+{'title': 'Great Customer Service\n', 'name': 'John\n', 'date': '2017-12-21\n', 'feedback': 'The customer service here is very good. They helped me find a 2017 Camry with good condition in reasonable price. Campared to other dealers, they provided the lowest price. Definttely recommend!\n'}
+----------------------
+----------------------
+ # QA: print(json.dumps(feedback_dictionary))
+{"title": "Great Customer Service\n", "name": "John\n", "date": "2017-12-21\n", "feedback": "The customer service here is very good. They helped me find a 2017 Camry with good condition in reasonable price. Campared to other dealers, they provided the lowest price. Definttely recommend!\n"}
+----------------------
+----------------------
+ # QA: print(post_each_1_feedback_in_json_format_2_server_double_quotes)
+<Response [400]>
+----------------------
+
+['Best experience so far\n', 'Katie\n', '2019-12-09\n', "My friend recommended this place and  I decided to give a try. I called them beforehand and made sure they have the models I was looking for. They evenly sent me a bunch of photos and specs of the car I was looking for. One the day I visited, the sales person John helped me with test drive and showed me what I should be aware of when buying a used car. I felt like I learned a lot. They made the process so smooth that I saved a lot of time. It's the best car purchasing experience I had so far!\n"]
+{'title': 'Best experience so far\n', 'name': 'Katie\n', 'date': '2019-12-09\n', 'feedback': "My friend recommended this place and  I decided to give a try. I called them beforehand and made sure they have the models I was looking for. They evenly sent me a bunch of photos and specs of the car I was looking for. One the day I visited, the sales person John helped me with test drive and showed me what I should be aware of when buying a used car. I felt like I learned a lot. They made the process so smooth that I saved a lot of time. It's the best car purchasing experience I had so far!\n"}
+----------------------
+----------------------
+ # QA: print(json.dumps(feedback_dictionary))
+{"title": "Best experience so far\n", "name": "Katie\n", "date": "2019-12-09\n", "feedback": "My friend recommended this place and  I decided to give a try. I called them beforehand and made sure they have the models I was looking for. They evenly sent me a bunch of photos and specs of the car I was looking for. One the day I visited, the sales person John helped me with test drive and showed me what I should be aware of when buying a used car. I felt like I learned a lot. They made the process so smooth that I saved a lot of time. It's the best car purchasing experience I had so far!\n"}
+----------------------
+----------------------
+ # QA: print(post_each_1_feedback_in_json_format_2_server_double_quotes)
+<Response [400]>
+----------------------
+
+['Waste of my time\n', 'Anonymous\n', '2018-09-21\n', "I came in around 6pm and they seemed about to close the store. One of the sales seemed not being patient with me and made me feel like I have to either buy a car or come back later. Of course I didn't buy a car there. Hopefully they can treat every customer with more patience.\n"]
+{'title': 'Waste of my time\n', 'name': 'Anonymous\n', 'date': '2018-09-21\n', 'feedback': "I came in around 6pm and they seemed about to close the store. One of the sales seemed not being patient with me and made me feel like I have to either buy a car or come back later. Of course I didn't buy a car there. Hopefully they can treat every customer with more patience.\n"}
+----------------------
+----------------------
+ # QA: print(json.dumps(feedback_dictionary))
+{"title": "Waste of my time\n", "name": "Anonymous\n", "date": "2018-09-21\n", "feedback": "I came in around 6pm and they seemed about to close the store. One of the sales seemed not being patient with me and made me feel like I have to either buy a car or come back later. Of course I didn't buy a car there. Hopefully they can treat every customer with more patience.\n"}
+----------------------
+----------------------
+ # QA: print(post_each_1_feedback_in_json_format_2_server_double_quotes)
+<Response [400]>
+----------------------
+
+['Good deal for a 2015 RAV4\n', 'Anonymous\n', '2018-04-17\n', 'Called them to look for a second-hand RAV4 and they are very nice and patience to help me find me a few matches then scheduled an appointmet with me. Came in and they had everything ready for me. I was surprised how professional those sales are and they explained and answered all my questions. Ended up buying the car and been using it for more than a month now. Everything looks good!\n']
+{'title': 'Good deal for a 2015 RAV4\n', 'name': 'Anonymous\n', 'date': '2018-04-17\n', 'feedback': 'Called them to look for a second-hand RAV4 and they are very nice and patience to help me find me a few matches then scheduled an appointmet with me. Came in and they had everything ready for me. I was surprised how professional those sales are and they explained and answered all my questions. Ended up buying the car and been using it for more than a month now. Everything looks good!\n'}
+----------------------
+----------------------
+ # QA: print(json.dumps(feedback_dictionary))
+{"title": "Good deal for a 2015 RAV4\n", "name": "Anonymous\n", "date": "2018-04-17\n", "feedback": "Called them to look for a second-hand RAV4 and they are very nice and patience to help me find me a few matches then scheduled an appointmet with me. Came in and they had everything ready for me. I was surprised how professional those sales are and they explained and answered all my questions. Ended up buying the car and been using it for more than a month now. Everything looks good!\n"}
+----------------------
+----------------------
+ # QA: print(post_each_1_feedback_in_json_format_2_server_double_quotes)
+<Response [400]>
+----------------------
+
+['You will find what you want here\n', 'Tom\n', '2019-06-05\n', "I've being look around for a second handed Lexus RX for my family and this store happened to have a few of those. The experience was similar to most car dealers. The one I ended up buying has good condition and low mileage. I am pretty satisfied with the price they offered.\n"]
+{'title': 'You will find what you want here\n', 'name': 'Tom\n', 'date': '2019-06-05\n', 'feedback': "I've being look around for a second handed Lexus RX for my family and this store happened to have a few of those. The experience was similar to most car dealers. The one I ended up buying has good condition and low mileage. I am pretty satisfied with the price they offered.\n"}
+----------------------
+----------------------
+ # QA: print(json.dumps(feedback_dictionary))
+{"title": "You will find what you want here\n", "name": "Tom\n", "date": "2019-06-05\n", "feedback": "I've being look around for a second handed Lexus RX for my family and this store happened to have a few of those. The experience was similar to most car dealers. The one I ended up buying has good condition and low mileage. I am pretty satisfied with the price they offered.\n"}
+----------------------
+----------------------
+ # QA: print(post_each_1_feedback_in_json_format_2_server_double_quotes)
+<Response [400]>
+----------------------
+
+----------------------
+----------------------
+ # QA: response.status_code
+400
+ # QA: response.ok
+False
+----------------------
+ # QA: response.request.url
+http://35.222.215.76/feedback/
+----------------------
+ # QA: response.request.body
+b'"{\\"title\\": \\"You will find what you want here\\\\n\\", \\"name\\": \\"Tom\\\\n\\", \\"date\\": \\"2019-06-05\\\\n\\", \\"feedback\\": \\"I\'ve being look around for a second handed Lexus RX for my family and this store happened to have a few of those. The experience was similar to most car dealers. The one I ended up buying has good condition and low mileage. I am pretty satisfied with the price they offered.\\\\n\\"}"'
+----------------------
+
+student-00-1f0759f0151d@corpweb:~$
+"""
